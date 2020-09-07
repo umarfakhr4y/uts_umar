0f542e1fa41531d13cb0e418db47b762198a681a
umarfakhr4y <umarfakhr4y@gmail.com>
2020-09-07 22:52:20 +0700

    pertama

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Ini Latihan Satu</title>
+  <title>Ini Latihan Commit</title>
   <meta content="" name="descriptison">
   <meta content="" name="keywords">
 
