3740ea975033b5a15f9e12302dac05269dfa8578
umarfakhr4y <umarfakhr4y@gmail.com>
2020-09-07 22:53:51 +0700

    kedua

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Ini Latihan Commit</title>
+  <title>Ini Latihan Commit kedua</title>
   <meta content="" name="descriptison">
   <meta content="" name="keywords">
 
